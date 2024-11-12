ec98dccdeede549aa2ab1ae1b10e24a294986504
dabarshi <dabarshiroy@gmail.com>
2024-11-13 02:55:51 +0600

    update cors

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// Replace with your frontend’s origin
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://ristorante-alfiero-server-n8l2utsv4.vercel.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Include if your requests require cookies/auth headers
+  };
+  
+  app.use(cors(corsOptions));
+
 // Routes
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/events', eventRoutes);
